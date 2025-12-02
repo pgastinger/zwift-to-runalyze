dd4890d649ff9ff8582e094438c273b736c8cd01
Peter Gastinger <peter.gastinger@gmail.com>
2025-12-02 21:56:05 +0100

    removed old files

--- test_garmin_sdk.py
+++
@@ -1,8 +0,0 @@
-from garmin_fit_sdk import Decoder, Stream
-
-stream = Stream.from_file("Activity.fit")
-decoder = Decoder(stream)
-messages, errors = decoder.read()
-
-print(errors)
-print(messages)
